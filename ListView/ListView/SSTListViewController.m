cb90d7974754a75f80585d38f2dff9b5b4b33052
Brennan Stehling <brennan@smallsharptools.com>
2013-08-02 12:45:25 -0500

    added second collection view to show how to show partial cells

--- ListView/ListView/SSTListViewController.m
+++ ListView/ListView/SSTListViewController.m
@@ -8,9 +8,10 @@
 
 #import "SSTListViewController.h"
 
-@interface SSTListViewController () <UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout>
+@interface SSTListViewController () <UICollectionViewDataSource, UICollectionViewDelegate>
 
-@property (weak, nonatomic) IBOutlet UICollectionView *collectionView;
+@property (weak, nonatomic) IBOutlet UICollectionView *firstCollectionView;
+@property (weak, nonatomic) IBOutlet UICollectionView *secondCollectionView;
 
 @property (strong, nonatomic) NSArray *items;
 
@@ -22,49 +23,82 @@
 #pragma mark -
 
 - (void)viewDidLoad {
-    DebugLog(@"%@", NSStringFromSelector(_cmd));
     [super viewDidLoad];
     
     self.items = @[@1, @2, @3, @4, @5, @6, @7, @8, @9, @10];
-    self.collectionView.allowsMultipleSelection = YES;
+    self.firstCollectionView.allowsMultipleSelection = YES;
+    self.secondCollectionView.allowsMultipleSelection = YES;
+    
+    // don't let the empty cells show
+    self.secondCollectionView.contentInset = UIEdgeInsetsMake(0, -200, 0, -200);
+}
+
+- (void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+    
+    if (self.secondCollectionView.contentOffset.x == 0) {
+        self.secondCollectionView.contentOffset = CGPointMake(150, 0);
+    }
+
+}
+
+#pragma mark - Private
+#pragma mark -
+
+- (BOOL)isSimpleCell:(UICollectionView *)collectionView atIndexPath:(NSIndexPath *)indexPath {
+    if (self.secondCollectionView == collectionView) {
+        // the first and last cell in the second collection view will use the empty cell
+        if (indexPath.row == 0 || indexPath.row == self.items.count + 1) {
+            return FALSE;
+        }
+    }
+    
+    return TRUE;
 }
 
 #pragma mark - UICollectionViewDataSource
 #pragma mark -
 
 - (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
-    return self.items.count;
+    if (self.firstCollectionView == collectionView) {
+        return self.items.count;
+    }
+    else {
+        return self.items.count + 2;
+    }
 }
 
 - (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
-    UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"SimpleCell" forIndexPath:indexPath];
-    
-    UILabel *label = (UILabel *)[cell viewWithTag:1];
-    NSNumber *number = self.items[indexPath.row];
-    label.text = [NSString stringWithFormat:@"%@", number];
-    label.textColor = cell.selected ? [UIColor lightGrayColor] : [UIColor whiteColor];
-    
-    return cell;
+    if ([self isSimpleCell:collectionView atIndexPath:indexPath]) {
+        NSUInteger index = indexPath.row - (self.secondCollectionView == collectionView ? 1 : 0);
+        MAAssert(self.items.count > index, @"Count must be greater than row");
+        UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"SimpleCell" forIndexPath:indexPath];
+        UILabel *label = (UILabel *)[cell viewWithTag:1];
+        NSNumber *number = self.items[index];
+        label.text = [NSString stringWithFormat:@"%@", number];
+        label.textColor = cell.selected ? [UIColor lightGrayColor] : [UIColor whiteColor];
+        
+        return cell;
+    }
+    else {
+        UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"EmptyCell" forIndexPath:indexPath];
+        return cell;
+    }
 }
 
 #pragma mark - UICollectionViewDelegate
 #pragma mark -
 
 - (void)collectionView:(UICollectionView *)collectionView didSelectItemAtIndexPath:(NSIndexPath *)indexPath {
-    DebugLog(@"selected %i", indexPath.row);
-    
-    UICollectionViewCell *cell = [self.collectionView cellForItemAtIndexPath:indexPath];
+    UICollectionViewCell *cell = [collectionView cellForItemAtIndexPath:indexPath];
     UILabel *label = (UILabel *)[cell viewWithTag:1];
     label.textColor = [UIColor lightGrayColor];
 }
 
 - (void)collectionView:(UICollectionView *)collectionView didDeselectItemAtIndexPath:(NSIndexPath *)indexPath {
-    UICollectionViewCell *cell = [self.collectionView cellForItemAtIndexPath:indexPath];
+    UICollectionViewCell *cell = [collectionView cellForItemAtIndexPath:indexPath];
     UILabel *label = (UILabel *)[cell viewWithTag:1];
     label.textColor = [UIColor whiteColor];
 }
-
-#pragma mark - UICollectionViewDelegateFlowLayout
-#pragma mark -
 
 @end
